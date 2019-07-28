a89df24b356e4c90288cecfe490a49e8c2bd225c
Raziel Amador <razielar@gmail.com>
2019-07-28 21:58:42 +0200

    first JavaScript script

---
+++ Week_2/variables.js
@@ -0,0 +1,32 @@
+
+
+var x=3;
+var y=4;
+var z= x+2*y;
+
+x=z-1;
+y=y*2;
+
+//var fgImage= new SimpleImage("perrito.jpg");
+
+//var w = fgImage.getWidth();
+//var h = fgImage.getHeight();
+
+print(x);
+
+function square(x) {
+    var ans= x*x;
+    return ans;
+}
+
+var y = square(4);
+
+
+
+
+
+
+
+
+
+
